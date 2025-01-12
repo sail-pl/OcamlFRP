197d6c40b97b98209f24945ffcfe8561d3c31955
Frédéric Dabrowski <33789275+DabrowskiFr@users.noreply.github.com>
2025-01-12 20:49:53 +0100

    cleaning

--- lib/arrows/arrows.mli
+++ lib/arrows/arrows.mli
@@ -26,17 +26,16 @@ val coiterate : ('s -> 'a * 's) -> 's -> 'a stream
     consumes streams of type ['a] and produce streams of type ['b] *)
 type ('a,'b) sf
 
+(** [apply f s] applies the stream function [f] to the stream [s] *)
 val apply : ('a,'b) sf -> 'a stream -> 'b stream
 
-(** The stream function [arr f] applies the function [f] to each element of a stream.
-    It satisfies the following equations :
-   - head ((arr f) s) = f (head s) 
-   - tail ((arr f) s) = arr f (tail s) *)
+(** The stream function [arr f] applies the function [f] to each element of a stream.*)
 val arr : 'a 'b. ('a -> 'b) -> ('a,'b) sf
 
 (** The unary [first] operator applies a stream function to the left members of a stream of pairs.*)
 val first : 'a  'b 'c. ('a, 'b) sf -> ('a * 'c, 'b * 'c) sf
 
+(** [f >>> g] is the sequential composition of stream functions [f] and [g] *)
 val (>>>) : ('a, 'b) sf -> ('b,'c) sf -> ('a, 'c) sf
 
 (** The unary [loop] operator plugs a stream function to a register *)            
--- playground/main.ml
+++
@@ -1,8 +0,0 @@
-
-
-(* let rec count : 'a 'b. ('a * 'b) -> int -> int =
-  function 
-    | (x,y) -> 1 
-    | (x,y) -> 1 
- *)
-(* retourner le nombre d'états augmentés *)
