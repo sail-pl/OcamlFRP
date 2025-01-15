e1b8f78559696abf017b37e7e03ae80d3b42a67f
Frédéric Dabrowski <33789275+DabrowskiFr@users.noreply.github.com>
2025-01-15 12:16:27 +0100

    refactor, add stream.ml

---
+++ lib/stream.ml
@@ -0,0 +1,43 @@
+open Coiterators
+
+type 'a stream = Str : ('a, 's) co -> 'a stream
+
+(* let stream_of_iterator (c : ('a,'s) co) : 'a stream = Str c
+let iterator_of_stream (Str c : 'a stream) :('a,'b) co  = c *)
+
+(* map *)
+
+(* let apply_in (f : ('a,'s) co -> ('b, 's2) co) (Str c : 'a stream) = 
+  Str (f c) *)
+
+let produce : 'a 's. ('s -> 'a * 's) -> 's -> 'a stream = 
+  fun h s -> Str (Co (h,s)) 
+
+let coiterate : 'a. ('a -> 'a) -> 'a -> 'a stream = 
+  fun f x0 -> produce (fun x -> let y = f x in (x,y)) x0
+
+let constant : 'a. 'a -> 'a stream =
+  fun x -> coiterate (Fun.const x) x
+
+let list_of_stream : 'a stream -> int -> 'a list =
+  fun (Str s : 'a stream) (n : int) : 'a list ->
+    to_list s n
+
+let rec perform : 'a stream -> ('a -> unit) -> int -> unit = 
+  fun s f n ->
+    if n <= 0 then ()
+    else 
+      let (Str ((Co (h,s)))) =  s in 
+      let (a,s') = h s in f a; 
+        perform (Str ((Co (h,s')))) f (n-1)
+
+let rec consume : 'a stream -> ('a -> bool) -> float option -> unit = 
+  fun s f d ->
+    let (Str ((Co (h,s)))) =  s in 
+    let (a,s') = h s in 
+    let b = f a in
+      (* print_string "a\n"; flush stdout; *)
+      match d with None -> () | Some t -> Thread.delay t;
+      if b then 
+        consume (Str ((Co (h,s')))) f d
+      else ()   
---
+++ lib/stream.mli
@@ -0,0 +1,25 @@
+(** {1 Streams} *)
+open Coiterators
+
+(** ['a stream] represents a type of stream of values of type ['a], 
+    which is an infinite sequence of values of type ['a]. *)
+type 'a stream = Str : ('a, 's) co -> 'a stream
+
+
+(** [coiterate f s] constructs a stream using the coiterator [(f, s)], 
+    where [f] is the function and [s] is the initial state. *)
+val produce : ('s -> 'a * 's) -> 's -> 'a stream
+
+(** [fold f x] generates a stream by iteratively applying the function [f] 
+    to the current value, starting with [x]. s*)
+    val coiterate : 'a. ('a -> 'a) -> 'a -> 'a stream
+    
+(** [constant x] returns a constant stream where every element has the value x. *)
+    val constant : 'a. 'a -> 'a stream
+    
+(** [list_of_stream s n] returns the first [n] elements of [s] as a list. *)
+    val list_of_stream : 'a stream -> int -> 'a list
+    val perform : 'a stream -> ('a -> unit) -> int -> unit
+
+    val consume : 'a stream -> ('a -> bool) -> float option -> unit
+    (* hd, tl ?? *)
