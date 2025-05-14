431870055d47978142f8b581211f8f5b72224cd3
Nicolas Paul <nicolas.paul1@etu.univ-orleans.fr>
2025-05-14 20:42:25 +0200

    Write documentation for Arrows module
    We were lacking some documentation about arrows, this is now changed.
    Also, with the incoming examples of using arrows, the usability of the
    library will significantly increase.

--- lib/arrows.ml
+++ lib/arrows.ml
@@ -1,151 +1,125 @@
-(*************************************************************************)
-(*                                                                       *)
-(*                                OCamlFRP                               *)
-(*                                                                       *)
-(* Copyright (C) 2025  Frédéric Dabrowski                                *)
-(* All rights reserved.  This file is distributed under the terms of      *)
-(* the GNU Lesser General Public License version 3.                      *)
-(* You should have received a copy of the GNU General Public License     *)
-(* along with this program.  If not, see <https://www.gnu.org/licenses/>.*)
-(*************************************************************************)
+(*****************************************************************************)
+(*                                                                           *)
+(*                                  OCamlFRP                                 *)(*                                                                           *)
+(* Copyright (C) 2025 Frédéric Dabrowski                                     *)
+(* Copyright (C) 2025 Nicolas Paul                                           *)
+(* All rights reserved.  This file is distributed under the terms of         *)
+(* the GNU Lesser General Public License version 3.                          *)
+(* You should have received a copy of the GNU General Public License         *)
+(* along with this program.  If not, see <https://www.gnu.org/licenses/>.    *)
+(*****************************************************************************)
 
 open Stream   
 
 type ('a, 'b) sf = 
-  SF : ('s -> 'a -> 'b * 's) * 's -> ('a, 'b) sf
+  | SF : ('s -> 'a -> 'b * 's) * 's -> ('a, 'b) sf
 
-let arr : ('a -> 'b) -> ('a, 'b) sf = 
-  fun f ->
-    let t, s = (fun () a -> (f a, ())), () in
-      SF (t, s)
+let arr f = SF ((fun () x -> f x, ()), ())
 
-let (>>>) : ('a, 'b) sf -> ('b, 'c) sf -> ('a, 'c) sf =
-    fun (SF (t1, s1)) (SF (t2, s2)) -> 
-      let t = 
-          fun (s1, s2) a -> 
-            let (b, s1') = t1 s1 a in 
-              let (c, s2') = t2 s2 b in 
-                (c, (s1', s2'))
-      and s = (s1, s2) in 
-        SF (t, s)
+let ( >>> ) (SF (f, sf)) (SF (g, sg)) =
+  let h (s1, s2) x =
+    let y, s1' = f s1 x in
+    let z, s2' = g s2 y in
+    z, (s1', s2')
+  in
+  SF (h, (sf, sg))
 
-let first : ('a, 'b) sf -> ('a * 'c, 'b * 'c) sf =
-  fun (SF (t, s)) -> 
-    let t = 
-      fun s (a, c) -> 
-        let (b, s') = t s a in ((b, c), s')
-    in SF (t,s)
+let first (SF (f, s)) =
+  let g s (x, z) = 
+    let y, s' = f s x in
+    (y, z), s'
+  in
+  SF (g, s)
     
-let second : ('a, 'b) sf -> ('c * 'a, 'c * 'b) sf =
-  fun (SF (t, s)) -> 
-    let t = 
-      fun s (c, a) -> 
-        let (b, s') = t s a in ((c, b), s')
-    in SF (t, s)
-    
-let parallel : ('a, 'b) sf -> ('c, 'd) sf -> ('a * 'c, 'b * 'd) sf = 
-  fun (SF (t1, s1)) (SF (t2, s2)) ->
-    let t =  
-      fun (s1, s2) (a, c) -> 
-        let (b, s1') = t1 s1 a
-        and (d, s2') = t2 s2 c in 
-          ((b, d), (s1', s2'))
-    in SF (t, (s1, s2))
+let second (SF (f, s)) =
+  let g s (z, x) = 
+    let y, s' = f s x in
+    (z, y), s'
+  in
+  SF (g, s)
 
-let fanout : ('a, 'b) sf -> ('a, 'c) sf -> ('a, 'b * 'c) sf =
-    fun (SF (t1, s1)) (SF (t2, s2)) ->
-      let t = 
-        fun (s1, s2) a -> 
-          let (b, s1') = t1 s1 a
-          and (c, s2') = t2 s2 a in 
-              ((b, c), (s1', s2'))
-        in SF (t, (s1, s2))    
-            
-let left : 
-  ('a, 'b) sf -> 
-    (('a, 'c) Either.t, ('b, 'c) Either.t) sf = 
-  fun (SF (t, s)) -> 
-    let t = fun s -> 
-      function 
-        | Either.Left a -> 
-            let (b, s') = t s a in 
-              (Either.Left b, s')
-        | Either.Right c -> (Either.Right c, s)
-    in SF (t, s)
+let parallel (SF (f, sf)) (SF (g, sg))  = 
+  let h (s1, s2) (x, y) =  
+    let x', s1' = f s1 x in
+    let y', s2' = g s2 y in 
+    (x', y'), (s1', s2')
+  in
+  SF (h, (sf, sg))  
 
-let right : 
-  ('a, 'b) sf -> 
-    (('c, 'a) Either.t, ('c, 'b) Either.t) sf = 
-  fun (SF (t, s)) -> 
-    let t = fun s -> 
-      function 
-        | Either.Left c -> (Either.Left c, s)
-        | Either.Right a -> 
-            let (b, state') = t s a in 
-              (Either.Right b, state')
-    in SF (t, s)
+let fanout (SF (f, sf)) (SF (g, sg)) =
+  let h (s1, s2) x =
+    let x1, s1' = f s1 x in
+    let x2, s2' = g s2 x in
+    (x1, x2), (s1', s2')
+  in
+  SF (h, (sf, sg))
 
-let choice : 
-  ('a, 'c) sf -> ('b, 'd) sf -> 
-    (('a, 'b) Either.t, ('c, 'd) Either.t) sf = 
-  fun (SF (t1, s1)) (SF (t2, s2)) -> 
-    let t = 
-      fun (state1, state2) -> 
-        function 
-          | Either.Left a -> 
-              let (c, state') = t1 s1 a in 
-                (Either.Left c, (state', state2))
-          | Either.Right b -> 
-            let (d, state') = t2 s2 b in 
-              (Either.Right d, (state1, state'))
-          in 
-            SF (t, (s1, s2))
+let left (SF (f, s)) =
+  let g s =
+    function
+    | Either.Left x ->
+      let x', s' = f s x in
+      Either.Left x', s' 
+    | Either.Right _ as x -> x, s 
+  in
+  SF (g, s)
 
-let fanin : 
-  ('a, 'c) sf -> ('b, 'c) sf -> 
-      (('a, 'b) Either.t, 'c) sf =
-  fun (SF (t1, s1)) (SF (t2, s2)) -> 
-    let t = 
-      fun (s1, s2) -> 
-        function 
-          | Either.Left a -> 
-            let (b, s') = t1 s1 a in 
-              (b, (s', s2))
-          | Either.Right c -> 
-            let (b, s') = t2 s2 c in 
-              (b, (s1, s'))
-    in SF (t, (s1, s2))
+let right (SF (f, s)) =
+  let g s =
+    function
+    | Either.Left _ as x -> x, s
+    | Either.Right x ->
+      let x', s' = f s x in
+      Either.Right x', s'
+  in
+  SF (g, s)
+
+let choice (SF (f, sf)) (SF (g, sg)) =
+  let h (s1, s2) =
+    function
+    | Either.Left x ->
+      let x', s1' = f s1 x in
+      Either.Left x', (s1', s2)
+    | Either.Right x ->
+      let x', s2' = g s2 x in
+      Either.Right x', (s1, s2')
+  in
+  SF (h, (sf, sg))
+
+let fanin (SF (f, sf)) (SF (g, sg)) =
+  let h (s1, s2) =
+    function
+    | Either.Left x ->
+      let x', s1' = f s1 x in
+      x', (s1', s2)
+    | Either.Right x ->
+      let x', s2' = g s2 x in
+      x', (s1, s2')
+  in
+  SF (h, (sf, sg))
         
-let loop : ('a * 'c, 'b * 'c) sf -> 'c -> ('a, 'b) sf =
-  fun (SF (t, s)) c ->
-    let t = 
-      fun (state, c) a ->
-        let (b, c'), state' = t state (a, c) in  
-          (b, (state', c'))
-    in SF (t, (s, c))
+let loop (SF (f, s)) x0 =
+  let h (s, c) a = 
+    let (b, c'), s' = f s (a, c) in  
+    (b, (s', c'))
+  in
+  SF (h, (s, x0))
 
-let lift : ('a, 'b) sf -> 'a stream -> 'b stream =
-  fun (SF (t1, s1)) -> 
-    fun (Stream (t2, s2)) -> 
-      let t = fun (state2, state1) -> 
-        let (a, state2') = t2 state2 in 
-          let (b, state1') = t1 state1 a in 
-            (b, (state2', state1'))
-      in Stream (t, (s2, s1))
+let lift (SF (f, sf)) (Stream (t, st)) =
+  let t (s2, s1) =
+    let (a, s2') = t s2 in 
+    let (b, s1') = f s1 a in 
+    (b, (s2', s1'))
+  in
+  Stream (t, (st, sf))
 
-module Arr = struct 
-
-  let id : ('a, 'a) sf = 
-    let t = fun s a -> (a, s) in SF (t, ())
+module Arr =
+  struct 
+    let id =  SF ((fun s x -> x, s), ())
   
-  let const : 'b -> ('a, 'b) sf = 
-    fun b -> 
-      let t = fun s _ -> (b, s) in SF (t, ())
-  
-  let dup : ('a, 'a * 'a) sf = 
-    let t = fun s a -> ((a, a), s) in SF (t, ())
+    let const x = SF ((fun s _ -> x, s), ())
 
-  let delay : 'a -> ('a, 'a) sf = 
-    fun a -> loop (arr Utils.swap) a
+    let dup = SF ((fun s x -> (x, x), s), ())
 
-end
+    let delay t = loop (arr Utils.swap) t
+  end
--- lib/arrows.mli
+++ lib/arrows.mli
@@ -1,63 +1,90 @@
-(*************************************************************************)
-(*                                                                       *)
-(*                                OCamlFRP                               *)
-(*                                                                       *)
-(* Copyright (C) 2025  Frédéric Dabrowski                                *)
-(* All rights reserved.  This file is distributed under the terms of      *)
-(* the GNU Lesser General Public License version 3.                      *)
-(* You should have received a copy of the GNU General Public License     *)
-(* along with this program.  If not, see <https://www.gnu.org/licenses/>.*)
-(*************************************************************************)
+(*****************************************************************************)
+(*                                                                           *)
+(*                                  OCamlFRP                                 *)(*                                                                           *)
+(* Copyright (C) 2025 Frédéric Dabrowski                                     *)
+(* Copyright (C) 2025 Nicolas Paul                                           *)
+(* All rights reserved.  This file is distributed under the terms of         *)
+(* the GNU Lesser General Public License version 3.                          *)
+(* You should have received a copy of the GNU General Public License         *)
+(* along with this program.  If not, see <https://www.gnu.org/licenses/>.    *)
+(*****************************************************************************)
 
 open Stream
 
-(** {1 Stream functions} *)
-
-(** Type of synchronous functions which consumes values of type [a]
-    and produces values of type [b] *)  
+(** {1 Synchronous functions} *)
 
 (** Synchronous function type *)
-type ('a,'b) sf = SF : ('s -> 'a -> 'b * 's) * 's -> ('a, 'b) sf
+type ('a, 'b) sf =
+  | SF : ('s -> 'a -> 'b * 's) * 's -> ('a, 'b) sf
 
-(** Lift a function to a synchronous function *)
+(** [arr f] is the synchronous function constructed from function [f]. *)
 val arr : ('a -> 'b) -> ('a,'b) sf
 
-(** Composition for synchronous functions *)
-val (>>>) : ('a, 'b) sf -> ('b,'c) sf -> ('a, 'c) sf
+(** [f >>> g] is the synchronous function composition of synchronous functions
+    [f] and [g]. *)
+val ( >>> ) : ('a, 'b) sf -> ('b,'c) sf -> ('a, 'c) sf
 
-(** First projection for synchronous functions *)
-val first : 'a  'b 'c. ('a, 'b) sf -> ('a * 'c, 'b * 'c) sf
+(** [first f] is the synchronous function which applies only to the first
+    element of a pair. *)
+val first : ('a, 'b) sf -> ('a * 'c, 'b * 'c) sf
 
-(** Second projection for synchronous functions *)
-val second : ('a,'b) sf -> ('c * 'a, 'c * 'b) sf
+(** [second f] is the synchronous function which applies only to the second
+    element of a pair. *)
+val second : ('a, 'b) sf -> ('c * 'a, 'c * 'b) sf
 
-(** Parallel composition for synchronous functions *)
+(** [parallel f g] is the synchronous function which applies synchronous
+    function [f] on the first element of a pair, and synchronous function [g]
+    on the second. *)
 val parallel : ('a, 'b) sf -> ('c, 'd) sf -> ('a * 'c, 'b * 'd) sf 
 
-(** Fanout for synchronous functions *)
+(** [fanout f g] is the synchronous function which applies synchronous
+    functions [f] and [g] on a single entry, returning a pair of both
+    results [(f x, g x)] *)
 val fanout : ('a, 'b) sf -> ('a, 'c) sf -> ('a, 'b * 'c) sf
 
-(** Left projection for synchronous functions *)
+(** [left f e] is the synchronous function which applies synchronous function
+    [f] to [Either.Left] [e]. *)
 val left : ('a, 'b) sf -> (('a, 'c) Either.t, ('b, 'c) Either.t) sf
 
-(** Right projection for synchronous functions *)
+(** [right f e] is the synchronous function which applies synchronous function
+    [f] to [Either.Right] [e]. *)
 val right : ('a, 'b) sf -> (('c, 'a) Either.t, ('c, 'b) Either.t) sf
 
-(** Choice for synchronous functions *)
-val choice : ('a, 'c) sf -> ('b, 'd) sf -> (('a, 'b) Either.t, ('c, 'd) Either.t) sf
+(** [choice f g] is the synchronous function which applies synchronous
+    function [f] on [Either.Left] [e] or synchronous function [g] on
+    [Either.Right] [e]. *)
+val choice : ('a, 'b) sf -> ('c, 'd) sf -> 
+             (('a, 'c) Either.t, ('b, 'd) Either.t) sf
 
-(** Fanin for synchronous functions *)
-val fanin : ('a, 'c) sf -> ('b, 'c) sf -> (('a, 'b) Either.t, 'c) sf
+(** [fanin f g] is the synchronous function which consumes an [Either.t]
+    and applies on the value synchronous function [f] if it was an
+    [Either.Left] or synchronous function [g] if it was an [Either.Right]. *)
+val fanin : ('a, 'b) sf -> ('c, 'b) sf -> (('a, 'c) Either.t, 'b) sf
 
-(** Loop for synchronous functions *)
+(** [loop f x0] is the synchronous function which calls itself with the
+    previous state, starting with state [x0]. *)
 val loop : ('a * 'x, 'b * 'x) sf -> 'x -> ('a, 'b) sf
 
 (** Iterate for synchronous functions *)
+(** [lift f s] is the stream produced by applying synchronous function
+    [f] to stream [s]. *)
 val lift : ('a,'b) sf -> 'a stream -> 'b stream
 
+(* TODO(nico): delete this nested module, or wrap everything in
+   a module Arrows to have the SF type be named [t] to follow
+   popular naming convention [Module.t] ??? *)
+
 module Arr : sig
+    (** [id] is the synchronous function which returns its arguments. *)
     val id : ('a, 'a) sf
+
+    (** [const x] is the synchronous function which always returns [x]. *)
     val const : 'b -> ('a, 'b) sf
+
+    (** [dup] is the synchronous function which returns a pair
+        of its arguments. *)
     val dup : ('a, 'a * 'a) sf
+
+    (** [delay t] *)
     val delay : 'a -> ('a, 'a) sf
 end
