365eaa1aa6296fcafad13d2e1f6ed335e3f90a0b
Nicolas Paul <nicolas.paul1@etu.univ-orleans.fr>
2025-05-20 10:25:15 +0200

    Use produce to construct Stream
    Instead of using the constructor, we use the function Stream.produce
    which offers two advantages over the constructor notation:
    * if Stream changes, produce will still be able to work, avoiding
      the need to change a lot of code; and
    * using produce offers a more functional code interface and make
      the source more elegant and easier to change.
    
    This alo have no impact on compilation or performance since all of
    this is inlined, in fact, this will help the compiler know that
    produce is a hot call to always be inlined!

--- lib/stream.ml
+++ lib/stream.ml
@@ -10,35 +10,27 @@
 (* along with this program.  If not, see <https://www.gnu.org/licenses/>.     *)
 (******************************************************************************)
 
-type 'a stream = 
-  | Stream : ('s -> ('a * 's)) * 's -> 'a stream
+type 'a stream = Stream : ('s -> ('a * 's)) * 's -> 'a stream
 
-let destr (Stream (f, s)) =
-  let x, s' = f s in
-  x, Stream (f, s')
+let produce f s = Stream (f, s) 
+
+let coiterate f x0 = produce (fun x -> x, f x) x0
+
+let destr (Stream (f, s)) = let x, s' = f s in x, produce f s'
 
 let head stream = fst (destr stream)
 
 let tail stream = snd (destr stream)
   
-let map f (Stream (t, q)) =
-  Stream (
-    (fun s ->
-      let x, s' = t s in
-      f x, s'),
-    q)
+let map f (Stream (t, q)) = produce (fun s -> let x, s' = t s in f x, s') q
 
 let apply (Stream (f, sf)) (Stream (g, sg)) =
-  Stream (
-    (fun (s1, s2) ->
-      let x, s1' = f s1 in
-      let y, s2' = g s2 in
-      (x y), (s1', s2')),
-    (sf, sg))
-
-let produce f s = Stream (f, s) 
-
-let coiterate f x0 = produce (fun x -> x, f x) x0
+  let h (s1, s2) =
+    let x, s1' = f s1 in
+    let y, s2' = g s2 in
+    (x y), (s1', s2')
+  in
+  produce h (sf, sg)
 
 let constant x = coiterate (Fun.const x) x
   
@@ -47,28 +39,21 @@ let rec perform (Stream (g, s)) f n =
   else
     let x, s' = g s in
     f x ;
-    perform (Stream (g, s')) f (n - 1)
+    perform (produce g s') f (n - 1)
 
 let rec consume (Stream (f, s)) p d =
   let x, s' = f s in
-  match d with
-  | None -> ()
-  | Some timer ->
-    Thread.delay timer ;
-    if p x then consume (Stream (f, s')) p d
-    else ()
+  if p x then (* p might have side-effects (see Engine) -- nico *)
+    match d with
+      | None -> ()
+      | Some timer ->
+        Thread.delay timer ;
+        consume (produce f s') p d
 
-let stream_of_list l x =
-  Stream (
-    (function
-      | [] as l -> x, l
-      | h :: t -> h, t),
-    l)
+let stream_of_list l x = produce (function | [] -> x, [] | h :: t -> h, t) l
 
 let rec list_of_stream (Stream (f, s)) n =
   if n > 0 then
     let x, s' = f s in
-    x :: (list_of_stream (Stream (f, s')) (n - 1))
-    (* TODO(nico): Make it totally recursive to obtain TCO? *)
-  else
-    []
+    x :: (list_of_stream (produce f s') (n - 1))
+  else []
